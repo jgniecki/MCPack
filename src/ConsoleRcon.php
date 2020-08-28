07b13b23b70020b1c7e77b1b4e4a892c2d144001
Jakub Gniecki <kubuspl@onet.eu>
2020-08-28 17:01:43 +0200

    Created ConsoleRcon.php

---
+++ src/ConsoleRcon.php
@@ -0,0 +1,51 @@
+<?php declare(strict_types=1);
+/**
+ * @author Jakub Gniecki
+ * @copyright Jakub Gniecki <kubuspl@onet.eu>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+namespace DevLancer\MCPack;
+
+
+use Thedudeguy\Rcon;
+
+/**
+ * Class ConsoleRcon
+ * @package DevLancer\MCPack
+ */
+class ConsoleRcon extends Rcon implements ConsoleInterface
+{
+    /**
+     * @param string $command
+     * @return bool
+     */
+    public function sendCommand($command): bool
+    {
+        if (!$this->isConnected())
+            $this->connect();
+
+        return (bool) parent::sendCommand($command);
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getResponse(): ?string
+    {
+        if ($this->isConnected())
+            return parent::getResponse();
+
+        return null;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isConnected(): bool
+    {
+        return parent::isConnected();
+    }
+}
